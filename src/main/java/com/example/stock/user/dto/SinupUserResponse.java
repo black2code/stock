4d34c864fbb5737f25d8f7f12beef5fa4f72a797
black2code <alsdnr1080@gmail.com>
2023-07-10 01:32:01 +0900

    Add : dto 구현

---
+++ src/main/java/com/example/stock/user/dto/SinupUserResponse.java
@@ -0,0 +1,11 @@
+package com.example.stock.user.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SinupUserResponse {
+    private Long id;
+    private String name;
+}
